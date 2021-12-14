190399c7550916267c3f4c97a92aacd4d0ce8653
brandondoris <brandonmdoris@yahoo.com>
2021-12-14 14:11:56 -0500

    added widgets

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 function App() {
   return (
 		<div className='app'>
 			<Header />
 
-			<div className='app_body'>
-        <Sidebar/>
-        <Feed />
-				{/* Feed */}
-				{/* Widgets */}
+		<div className='app_body'>
+        	<Sidebar/>
+       		<Feed />
+			<Widgets />
 			</div>
 		</div>
 	);
