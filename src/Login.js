1c9bd74009f3145cfd95fbd3ee32af205ff012ef
brandondoris <brandonmdoris@yahoo.com>
2021-12-14 16:10:27 -0500

    added google authentication

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,26 @@
 import { Button } from '@mui/material';
 import React from 'react';
 import './Login.css'
+import { auth } from './firebase';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 function Login() {
 
-    const signIn = () => {
-        // sign in ...
-    }
+     const signIn = () => {
+		// sign In
+                const provider = new GoogleAuthProvider();
+					signInWithPopup(auth, provider)
+					.then((result) => {
+						console.log(result.user);
+					})
+					.catch((error) => alert(error.message));
+			};
     return (
 			<div className='login'>
 				<div className='login_logo'>
 					<img src='https://i.imgur.com/bbKKvPv.png' alt='devbranch-logo-transparent' />
 				</div>
-				<Button type='submit' oncClick={signIn}>
+				<Button type="submit" onClick={signIn}>
 					Sign In
 				</Button>
 			</div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyCXrPtEOBZrlkcFfyTdWrsgP6mOpuBYD2w',
 	authDomain: 'devbranch-e7972.firebaseapp.com',
@@ -7,3 +10,15 @@ const firebaseConfig = {
 	appId: '1:998539719469:web:2eea2ae11d36db854959a4',
 	measurementId: 'G-900LXFFTR0',
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+// const provider = new firebase.auth.GoogleAuthProvider();
+
+// export { auth, provider };
+// export default db;
